c98a8352b47d3186abbeab677872ef278484c460
aburaza3639 <aburaza3639@gmail.com>
2025-03-18 23:38:54 +0800

    Recreate

---
+++ AndroMoney/settings.py
@@ -0,0 +1,3 @@
+xlsx_FILE_PATH: str = "/Users/aburaza3639/Documents/プライベート/資産/AndroMoney.xlsx"
+TABLE_FILE_PATH: str = "/Users/aburaza3639/Documents/プライベート/資産/AndroMoney_Pivot.xlsx"
+xlsx2_file_path: str = "/Users/aburaza3639/Documents/プライベート/資産/クレジットカード履歴.xlsx"
